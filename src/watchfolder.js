9ebfccd3392bba0f63e1306bef37d0504a03fc42
Dieter Van Hoof <dieter.vanhoof@c4j.be>
2017-07-12 09:17:35 +0200

    Create directories (processing, refused, incomplete) when they don't exist. Make these with the same permissions and owner/group as the parent folder.

--- src/watchfolder.js
+++ src/watchfolder.js
@@ -1,6 +1,7 @@
 const chokidar = require("chokidar");
 const log = require("./services/logger.service");
 const FileIndex = require("./util/fileIndex");
+const FileUtils = require('./util/fileUtils');
 const FileRecognizer = require("./util/fileRecognizer");
 const options = require("./util/cmdargs").parseArguments();
 const Publisher = require("./amqp/publisher");
@@ -11,8 +12,18 @@ const generator = new Generator(options);
 const publisher = new Publisher(options);
 const fileindex = new FileIndex(options, new FileRecognizer(options), publisher, generator);
 
-log.success('Watching folder: ' + options.folder);
-chokidar.watch(options.folder,
+
+let parentFolderStat = fs.statSync(options.folder);
+let uid = parentFolderStat.uid;
+let gid = parentFolderStat.gid;
+let mode = parentFolderStat.mode;
+
+FileUtils.createDirectory(FileUtils.createFullPath(options.folder, options.PROCESSING_FOLDER_NAME), uid, gid, mode);
+FileUtils.createDirectory(FileUtils.createFullPath(options.folder, options.INCOMPLETE_FOLDER_NAME), uid, gid, mode);
+FileUtils.createDirectory(FileUtils.createFullPath(options.folder, options.REFUSED_FOLDER_NAME), uid, gid, mode);
+
+chokidar.watch(
+    options.folder,
     {
         ignored: (path) => {
             // Ignore sub-folders
@@ -24,4 +35,6 @@ chokidar.watch(options.folder,
     })
     .on('add', (path) => {
         fileindex.add_file(path, fileindex.determine_file_type(path, options), options, publisher, generator);
-    });
+    }
+);
+log.success('Watching folder: ' + options.folder);
